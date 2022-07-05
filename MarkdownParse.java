db20a045149adb5a93e8819b5113f52cc7ee73b8
Yihong-Li <yihong021@gmail.com>
2022-07-05 09:13:49 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -72,7 +72,10 @@ public class MarkdownParse {
                 return toReturn;
             }
             String potentialLink = markdown.substring(openParen + 1, closeParen).trim();
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
+            if(potentialLink.indexOf(" ") == -1 && 
+                potentialLink.indexOf("\n") == -1 &&
+                potentialLink.contains("com") &&
+                potentialLink.contains("html")) {
                 toReturn.add(potentialLink);
                 currentIndex = closeParen + 1;
             }
